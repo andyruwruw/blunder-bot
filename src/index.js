079ee4dc687079a6f4c187e47ad1c9cfa02c037d
Andrew Young <andrew@youngshome.com>
2020-02-04 01:57:35 -0700

    Documentation

--- src/index.js
+++ src/index.js
@@ -23,25 +23,24 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
-function check() {
-
-}
-
-bot.on('message', function (user, userID, channelID, message, evt) {
+bot.on('message', async function (user, userID, channelID, message, evt) {
 
     let check = () => {
         bot.sendMessage({
             to: channelID,
             message: 'MATE!'
         });
+        // console.log(user.servers);
     };
 
     let game = async (args) => {
-        let response = await axios.get('https://api.chess.com/pub/player/andyruwruw/games');
-        console.log(response.data);
+        // let response = await axios.get('https://api.chess.com/pub/player/andyruwruw/games');
+        // console.log(response.data);
     };
 
-    let help = async() => {
+    let help = async () => {
+        // Will Fix Formating
+        // Do Last
         await bot.sendMessage({to: channelID, message: 'Discoveries:\n`!help` - for BlunderBot Commands.\n`!game <player1> <player2> <gameID>` - Start tracking a game. GameID is in the URL or '});
     }
 
@@ -49,22 +48,41 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         await bot.sendMessage({to: channelID, message: 'That\'s a Blunder.\nTry `!help` for options.'});
     };
 
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
+    if (message.substring(0, 3) == 'bb!') {
+        var args = message.substring(3).split(' ');
         var cmd = args[0];
         args = args.splice(1);
 
         switch(cmd) {
             // !check
+            // Says Mate
             case 'check':
                 check();
                 break;
-            // !game
+            // !game <player_name> <player_name> <gameID>
+            // Creates new channel for tracking and talking about game
+            // Displays results and saves them to database.
             case 'game':
                 game(args);
                 break;
             // !help
+            // Says Instructions
             case 'help':
+                help();
+                break;
+            // !player <player_name>
+            // Displays win loss ratio to other players
+            case 'player':
+                help();
+                break;
+            // !archive <player_name> <index>
+            // Shows all games by player
+            case 'archive':
+                help();
+                break;
+            // !tournament <player_names>
+            // Starts new channels for tournament
+            case 'tournament':
                 help();
                 break;
             // !invalid
